606765c30fb806f2bac985072ee25164685000d0
indiomedeiros <bboyindio@gmail.com>
2021-02-23 16:23:19 -0300

    The validated image creation endpoint has been implemented

---
+++ src/business/entities/Image.ts
@@ -0,0 +1,19 @@
+export class Image {
+  constructor(
+    public readonly id: string,
+    public readonly subtitle: string,
+    public readonly author: string,
+    public readonly date: Date,
+    public readonly file: string,
+    public readonly tags: string[],
+    public readonly collection: string
+  ) {}
+}
+
+export interface ImageInputDTO {
+  subtitle: string;
+  file: string;
+  tags: string[];
+  collection: string;
+  token: string
+}
---
+++ src/controller/ImageController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { ImageInputDTO } from "../business/entities/Image";
+import { ImageBusiness } from "../business/ImageBusiness";
+import { Autheticator } from "../business/service/Authenticator";
+import { IdGenerator } from "../business/service/IdGenerator";
+import { ImageDatabase } from "../data/ImageDatabase";
+
+const imageBusiness = new ImageBusiness(
+  new Autheticator(),
+  new IdGenerator(),
+  new ImageDatabase()
+);
+
+export class ImageController {
+  public async createImage(req: Request, res: Response): Promise<void> {
+    try {
+      const { subtitle, file, tags, collection } = req.body;
+      const token = req.headers.authorization as string;
+      const imageInputDTO: ImageInputDTO = {
+        subtitle,
+        file,
+        tags,
+        collection,
+        token,
+      };
+
+      const message: string = await imageBusiness.createImage(imageInputDTO);
+
+      res.status(200).send({ message });
+    } catch (error) {
+      res
+        .status(error.statusCode || 400)
+        .send(error.sqlMessage || error.message);
+    }
+  }
+}
